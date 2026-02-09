3b701ef1f048c3798df8a67cd0e79588bbda8f07
Hopicekcz <Hopicekcz@post.cz>
2026-02-10 00:15:20 +0100

    AI system 0.1.7 - fixed all states, initial shoot function

--- devGame/Assets/Game/Scripts/aiNAV.cs
+++ devGame/Assets/Game/Scripts/aiNAV.cs
@@ -9,13 +9,20 @@ public class aiNAV : MonoBehaviour
     [Header("References")] //References for use by the script
     [SerializeField] private Transform playerTransform; //the playerCapsule is used for the playerTransform
     [SerializeField] private NavMeshAgent navAgent; //navigation Agent module for easier scripting
-    [SerializeField] private Transform firePoint; //The empty gameobject from where to fire bullets
     [SerializeField] private Animator animator; 
-
+    [SerializeField] private GameObject eyes;
+    [SerializeField] private Transform playerEyes;
+    [SerializeField] private Transform playerBody;
     [Header("Layers")]
     [SerializeField] private LayerMask terrainLayer; //layerMask for the Ground to determine walkable checks
     [SerializeField] private LayerMask playerLayerMask; //layerMask for the Player to determine where the player is
     [SerializeField] private LayerMask obstacleLayerMask; 
+    [SerializeField] private GameObject muzzleFlashPrefab;
+    [SerializeField] private Transform muzzlePoint;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip shootSound;
+    [SerializeField]private AudioSource audioSource;
 
     [Header("Patrol Settings")] //customizable setting of the patrol radius
     [SerializeField] private float patrolRadius; //the patrol radius
@@ -25,18 +32,18 @@ public class aiNAV : MonoBehaviour
     
 
     [Header("Combat Settings")] //customizable settings of the bullet and attack speed using rigidbody physics
-    [SerializeField] private float attackCooldown = 1f;
+    [SerializeField] private float attackCooldown = 2f;
     private int randomStrafe;
     [SerializeField] private float npcHP = 100f;
     [SerializeField] private float walkSpeed = 2f;
     [SerializeField] private float hitTimer = 2f;
     [SerializeField] private float patrolPointTimer = 2.5f;
     [SerializeField] private float staggeredSpeed = 1f;
-    [SerializeField] private float followPlayerTimer = 15f;
+    [SerializeField] private float followPlayerTimer = 1f;
 
     [Header("Detection Ranges")] //customizable settings for the vision (follow player) and engagement (attack) range
-    [SerializeField] private float visionRange = 10f;
-    [SerializeField] private float engagementRange = 7f;
+    [SerializeField] private float visionRange = 15f;
+    [SerializeField] private float engagementRange = 6f;
     [SerializeField] private float tooCloseRange = 3f;
 
     //Declaration of bools for behavior states
@@ -48,28 +55,30 @@ public class aiNAV : MonoBehaviour
     private bool isOnStrafePoint;
     private bool fightMode;
     private bool patrolTimerFinished = true;
-    private bool sawPlayer;
+    private bool releaseChase = true;
+    private bool keepChasing;
+    private string currentState;
+    private bool attackedPlayer = false;
     //Ray for checking collisions between the NPC and the Player
-    Ray npcToPlayerRay;
+    Ray npcToPlayerEyesRay;
+    Ray npcToPlayerBodyRay;
     Ray raySide;
     RaycastHit hitWall;
     RaycastHit hitGround;
     private Vector3 playerLastPosition;
 
     //TO ADD NEXT!!! - isPlayerVisible and isPlayerInRange checks need to be sent from an empty gameobject of the npc head to the player head (obstacles make player invisible)
-    //readd shortterm chase memory, but keep the previous point to get to. there should just be a timer once the player cant be seen, then once the timer is up the npc should go to the last known point.
     //atleast optimize a bit
     //comment a lot!!!!!
 
     //INITIALIZATION
     private void Awake(){ //Because the EnemyNPC is a prefab and the playertransform cannot be assigned into the reference, a script hard-reference is needed
-        if (playerTransform == null){
             GameObject playerObj = GameObject.Find("PlayerCapsule");
-            if (playerObj != null){
-                playerTransform = playerObj.transform;
-            }
-        }
-
+            playerTransform = playerObj.transform;
+            GameObject playerObjEyes = GameObject.Find("PlayerCameraRoot");
+            playerEyes = playerObjEyes.transform;
+            GameObject playerObjBody = GameObject.Find("PlayerBody");
+            playerBody = playerObjBody.transform;
         if(navAgent == null){
             navAgent = GetComponent<NavMeshAgent>();
         }
@@ -93,6 +102,7 @@ public class aiNAV : MonoBehaviour
 
     //MAIN FUNCTIONS
     private void Update(){ //Constant activation of functions
+    Debug.Log(currentState);
         DetectPlayer();
         UpdateBehaviourState();
         SetAnimation();
@@ -133,24 +143,30 @@ public class aiNAV : MonoBehaviour
 
     private void DetectPlayer()
     {
-        npcToPlayerRay = new Ray(transform.position, ((playerTransform.position - transform.position).normalized)); //assigning properties to the Raycast, being the instance´s position and the direction of a normalized 3D-vector (from the object to the player)
+        npcToPlayerEyesRay = new Ray(eyes.transform.position, ((playerEyes.transform.position - eyes.transform.position).normalized));
+        npcToPlayerBodyRay = new Ray(eyes.transform.position, ((playerBody.transform.position - eyes.transform.position).normalized)); //assigning properties to the Raycast, being the instance´s position and the direction of a normalized 3D-vector (from the object to the player)
 
-        isPlayerVisible =  !(Physics.Raycast(npcToPlayerRay, out RaycastHit hitVisible, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, visionRange, playerLayerMask);
-        isPlayerInRange = !(Physics.Raycast(npcToPlayerRay, out RaycastHit hitRange, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, engagementRange, playerLayerMask);
+        isPlayerVisible =  (!(Physics.Raycast(npcToPlayerEyesRay, out RaycastHit hitVisibleObstacle, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, visionRange, playerLayerMask)) && (!(Physics.Raycast(npcToPlayerEyesRay, out RaycastHit hitVisibleGround, (Vector3.Distance(transform.position, playerTransform.position)), terrainLayer)) && Physics.CheckSphere(transform.position, visionRange, playerLayerMask)) || (!(Physics.Raycast(npcToPlayerBodyRay, out RaycastHit hitVisibleObstacleBody, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, visionRange, playerLayerMask)) && (!(Physics.Raycast(npcToPlayerBodyRay, out RaycastHit hitVisibleGroundBody, (Vector3.Distance(transform.position, playerTransform.position)), terrainLayer)) && Physics.CheckSphere(transform.position, visionRange, playerLayerMask));
+        isPlayerInRange = (!(Physics.Raycast(npcToPlayerEyesRay, out RaycastHit hitRangeObstacle, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, engagementRange, playerLayerMask)) && (!(Physics.Raycast(npcToPlayerEyesRay, out RaycastHit hitRangeGround, (Vector3.Distance(transform.position, playerTransform.position)), terrainLayer)) && Physics.CheckSphere(transform.position, engagementRange, playerLayerMask)) || (!(Physics.Raycast(npcToPlayerBodyRay, out RaycastHit hitRangeObstacleBody, (Vector3.Distance(transform.position, playerTransform.position)), obstacleLayerMask)) && Physics.CheckSphere(transform.position, engagementRange, playerLayerMask)) && (!(Physics.Raycast(npcToPlayerBodyRay, out RaycastHit hitRangeGroundBody, (Vector3.Distance(transform.position, playerTransform.position)), terrainLayer)) && Physics.CheckSphere(transform.position, engagementRange, playerLayerMask));
         //variables for determining behaviour state - Raycast for checking if there are any obstacles between the npc and the player, CheckSphere for checking if the npc is in range of the player.
         //the RaycastHit variables are declared inside the function, to avoid unnecessary variable declaration in the initialization.
     }
 
     private void UpdateBehaviourState(){ //Behaviour state switcher
-     Debug.Log(navAgent.destination);
-        if(!isPlayerVisible && !isPlayerInRange && sawPlayer){ //Cant see player, Player isnt close and Player was not being chased = Patrol
-            PerformPatrol();
-        }
-        else if ((isPlayerVisible && !isPlayerInRange) || (!sawPlayer)){  //Can see player but is not close OR Cant see player, is not close but was being chased = Chase
-            PerformChase();
-        }
-        else if (isPlayerVisible && isPlayerInRange && sawPlayer){ //Can see player, is close = Attack
+        
+        if (isPlayerVisible && isPlayerInRange && releaseChase){ //Can see player, is close = Attack
             PerformAttack();
+            currentState = "Attack";
+        }
+        else if ((isPlayerVisible && !isPlayerInRange) || (!releaseChase) || (attackedPlayer)){  //Can see player but is not close OR Cant see player, is not close but was being chased = Chase
+            PerformChase();
+            currentState = "Chase";
+            Debug.Log("is it release chase" + !releaseChase);
+            Debug.Log("is it attackedPlayer" + attackedPlayer);
+        }
+        else if(!isPlayerVisible && !isPlayerInRange && !attackedPlayer){ //Cant see player, Player isnt close and Player was not being chased = Patrol
+            PerformPatrol();
+            currentState = "Patrol";
         }
     }
     //MAIN FUNCTIONS
@@ -200,16 +216,34 @@ public class aiNAV : MonoBehaviour
     private void PerformChase(){
         fightMode = false;
         isMoving = true;
-        if(isPlayerVisible && !isPlayerInRange){
-            sawPlayer = false;
+        if(attackedPlayer){
+            releaseChase = false;
             playerLastPosition = playerTransform.position;
-        } else if(!isPlayerVisible  && (Vector3.Distance(transform.position, playerLastPosition) < 0.1f) || isPlayerInRange){
-            sawPlayer = true;
+            attackedPlayer = false;
+            keepChasing = true;
+            StartCoroutine(KeepChasingTimer());
+        }
+        if(isPlayerVisible && !isPlayerInRange){
+            releaseChase = false;
+            playerLastPosition = playerTransform.position;
+            keepChasing = true;
+            StartCoroutine(KeepChasingTimer());
+            //once this stops being true start coroutine after that finishes start the code below
+        } 
+        if(keepChasing){
+            playerLastPosition = playerTransform.position;
+        }
+        if(!isPlayerVisible && (Vector3.Distance(transform.position, playerLastPosition) < 0.1f) && !keepChasing || isPlayerInRange ){
+            releaseChase = true;
         }
         navAgent.SetDestination(playerLastPosition);
          //simple follow the player
     }
 
+    private IEnumerator KeepChasingTimer(){
+        yield return new WaitForSeconds(followPlayerTimer);
+        keepChasing = false;
+    }
     
     //CHASE
     
@@ -217,20 +251,28 @@ public class aiNAV : MonoBehaviour
     private void PerformAttack(){
         isMoving = true;
         fightMode = true;
-        //if(!isOnAttackCooldown){ //attack function along with attack cooldown function
+        attackedPlayer = true;
+        AttackMovement();
+        if(!isOnAttackCooldown){ //attack function along with attack cooldown function
+            StartCoroutine(AttackCooldownRoutine());
             FireWeapon();
-            AttackMovement();
-            AttackCooldownRoutine();
-        //}
+        }
     }
 
     private void FireWeapon(){
-        
+        audioSource.PlayOneShot(shootSound);
+        StartCoroutine(ShootEffects());
+    }
+
+    private IEnumerator ShootEffects(){
+        GameObject muzzleSmoke = Instantiate(muzzleFlashPrefab, muzzlePoint);
+        yield return new WaitForSeconds(5);
+        Destroy(muzzleSmoke);
     }
 
     private void AttackMovement(){
         if(Physics.CheckSphere(transform.position, tooCloseRange, playerLayerMask)){
-            transform.LookAt(playerTransform);
+            transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
             navAgent.SetDestination(transform.position - transform.forward); 
         } else {
                 randomStrafe = Random.Range(0, 2);
@@ -239,7 +281,7 @@ public class aiNAV : MonoBehaviour
                 } else {
                     raySide = new Ray(transform.position + transform.right * 3, Vector3.down);
                 }
-                transform.LookAt(playerTransform);
+                transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
                 if(!isOnStrafePoint){
                     StartCoroutine(FindStrafePoint());
                 }
@@ -250,8 +292,7 @@ public class aiNAV : MonoBehaviour
             if((Physics.Raycast(raySide, out hitGround, 3f, terrainLayer)) && !isOnStrafePoint){
                     isOnStrafePoint = true;
                     navAgent.SetDestination(hitGround.point);
-                    transform.LookAt(playerTransform);
-                    Debug.DrawLine(raySide.origin, hitGround.point);
+                    transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
                     yield return new WaitForSeconds(1f);
                     isOnStrafePoint = false;
                     }        
